9e81098b5c35251f56ee16ded3bbf6aca99c337b
Austin Seipp <mad.one@gmail.com>
2008-06-14 07:06:41 -0500

    build infrastructure fleshed out, basic main() with command line options
    main.c now uses getopt_long to parse command line options, and for the
    most part the build infrastructure is working.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <llvm-c/Core.h>
 #include "config.h"
 
+static struct option longopts[] = {
+  { "verbose", no_argument, NULL, 'v'},
+  { "version", no_argument, NULL, 'V'},
+  { "help",    no_argument, NULL, 'h'},
+  { NULL, 0, NULL, 0 }
+};
+
+void version() {
+  printf(PACKAGE_STRING ": simple LLVM virtual machine.\n");
+  exit(0);
+}
+
+void usage() {
+  printf("usage: executable [options] [file]...\n");
+  printf("'file' is a bytecode file to load.\n");
+  printf("available options:\n");
+  printf(" --verbose\t-v\t\tincrease verbosity\n");
+  printf(" --version\t-V\t\tshow version information\n");
+  printf(" --help\t\t-h\t\tshow this information\n");
+  exit(0);
+}
+
 int main(int ac, char **av) {
-  printf("hello world!\n");
-  return 42;
+  int c;
+  int verbosity = 0;
+
+  while((c = getopt_long(ac,av,"vVh", longopts, NULL)) != -1) {
+    switch(c) {
+    case 'v':
+      printf("Verbosity is at %d.\n",++verbosity);
+      break;
+    case 'V':
+      version();
+    case 'h':
+    case '?':
+    default:
+      usage();
+    }
+  }
+
+  ac -= optind;
+  av += optind;
+
+  if(1 > ac) {
+    printf("Need a file.\n");
+  } else {
+    printf("Loading file %s...\n",av[0]);
+  }
+
+  return 0;
 }
